8193ca03be99b5e38d4d8905d5934c4c946b22e7
atzina <atzina@gmail.com>
2023-07-17 16:21:07 -0600

    barra de navegacion inicial

---
+++ tecnologia_y_discapacidad/src/components/Navar/Navar.jsx
@@ -0,0 +1,44 @@
+
+function Navar(){
+
+    return (
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/"> 
+                    {'Inicio'}
+                </a>
+            </li>
+            <li>
+                <a href="/diseno"> 
+                    {'Diseño inclusivo'}
+                </a>
+            </li>
+            <li>
+                <a href="/trabajo"> 
+                    {'Trabajo'}
+                </a>
+            </li>
+            <li>
+                <a href="/directorio"> 
+                    {'Directorio de proyectos y tencologías'}
+                </a>
+            </li>
+            <li>
+                <a href="/modelo"> 
+                    {'Modelo antiopresión'}
+                </a>
+            </li>
+            <li>
+                <a href="/recomendaciones"> 
+                    {'Recomendaciones'}
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
+    )
+}
+
+export default Navar;
