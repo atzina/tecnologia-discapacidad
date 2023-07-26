f7f186b5c6186e1c74a085365a7f1ad8bc2b0557
atzina <atzina@gmail.com>
2023-07-25 23:02:57 -0600

    primeros pasos la maquina del rango

--- tecnologia_y_discapacidad/src/components/ModAntiop/Rango.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Rango.jsx
@@ -2,7 +2,18 @@
 function Rango () {
 
     return (
-        <h1>Esto es el rango</h1>
+        <>
+        <div className="title-rango">
+        <h2>Esto es el rango</h2>
+        </div>
+        <p>
+            Podemos visualizar el Rango como una gran máquina de clasificación, un escáner de código de barras en un supermercado.
+            El escáner lee el código de nueve dígitos de cada persona para determinar sus roles de grupo exluido o grupo privilegiado. Esto sucede muy rápido, automáticamente. El escáner no se detine para investigar si la codificación realmente se ajusta al persona, simplemente elige una y continúa. El punto aquí es que el rango no es una identidad en evolución, es simplemente una categoría asignada o adscrita.<br></br>
+            La máquina fue diseñada y fabricada hace mucho tiempo para hacer solo una cosa; excluir al mayor número de personas or la menor razón posible.
+        </p>
+        <div><iframe src="https://giphy.com/embed/l1J9OTsRKPqjj2Y5q" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ciscoengemojis-security-l1J9OTsRKPqjj2Y5q">via GIPHY</a></p></div>
+        
+        </>
     )
 }
 
