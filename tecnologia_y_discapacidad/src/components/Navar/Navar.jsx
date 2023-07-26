4ad1631695529031f06aadf6ff92e6abe19bb76b
atzina <atzina@gmail.com>
2023-07-25 22:08:31 -0600

    version arreglo uno navbar collapse

--- tecnologia_y_discapacidad/src/components/Navar/Navar.jsx
+++ tecnologia_y_discapacidad/src/components/Navar/Navar.jsx
@@ -6,32 +6,31 @@ function Navar() {
     <header className="menu">
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <div className="div-icon">
-            <img className="icon-nav" src={Icon}></img>
-          </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="/"><img className="icon-nav" src={Icon}></img></a>
+      
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse div-nav" id="navbarNavDropdown">
-          <ul class="navbar-nav ">
-            <li>
+    <div class="collapse navbar-collapse div-nav" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className= "nav-item">
               <a className="nav-link nav-link-blue"  href="/">{"Inicio"}</a>
             </li>
-            <li>
+            <li className= "nav-item">
             <a class="nav-link nav-link-yellow" href="/modelo">{"Antiopresión"}</a>
             </li>
-            <li >
+            <li className= "nav-item">
               <a class="nav-link nav-link-pink" href="/trabajo">{"Trabajo"}</a>
             </li >
-            <li >
+            <li className= "nav-item">
               <a class="nav-link nav-link-red" href="/directorio">
                 {"Directorio de tencologías"}
               </a>
             </li>
-            <li >
+            <li className= "nav-item" >
               <a class="nav-link nav-link-aqua" href="/diseno">{"Diseño inclusivo"}</a>
             </li>
-            <li >
+            <li className= "nav-item">
               <a class="nav-link nav-link-brown" href="/recomendaciones">{"Recomendaciones"}</a>
             </li>
           </ul>
