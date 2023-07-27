d7d519c0d7b3ff9da7c7b4eabc4e55f74ceadaa0
atzina <atzina@gmail.com>
2023-07-26 18:48:07 -0600

    La maquina del rango completa primera versión

--- tecnologia_y_discapacidad/src/components/ModAntiop/Rango.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Rango.jsx
@@ -1,17 +1,53 @@
+import './Rango.css'
 
 function Rango () {
 
     return (
         <>
         <div className="title-rango">
-        <h2>Esto es el rango</h2>
+        <h2>La Máquina del Rango</h2>
         </div>
-        <p>
-            Podemos visualizar el Rango como una gran máquina de clasificación, un escáner de código de barras en un supermercado.
-            El escáner lee el código de nueve dígitos de cada persona para determinar sus roles de grupo exluido o grupo privilegiado. Esto sucede muy rápido, automáticamente. El escáner no se detine para investigar si la codificación realmente se ajusta al persona, simplemente elige una y continúa. El punto aquí es que el rango no es una identidad en evolución, es simplemente una categoría asignada o adscrita.<br></br>
-            La máquina fue diseñada y fabricada hace mucho tiempo para hacer solo una cosa; excluir al mayor número de personas or la menor razón posible.
-        </p>
-        <div><iframe src="https://giphy.com/embed/l1J9OTsRKPqjj2Y5q" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ciscoengemojis-security-l1J9OTsRKPqjj2Y5q">via GIPHY</a></p></div>
+        <section className='contenedor-rango'>
+        <div className= "contenedor-parrafouno">
+            <p>
+                El Rango es una de las capas que usa el Modelo de Leticia Nieto, para explicar descifrar nuestras interacciones complejas con el mundo y con ellas nuestra posibilidad de actuar antiopresivamente. El Estatus, el Rango y el Poder, son las capas que componen el modelo en su integridad. En este espacio nos centraremos en el Rango que es la capa que tiene que ver con nuestras membresías asignadas socialmente y dentro de las que específicamente podemos referirnos a la categoría de discapacidad. Una vez más recomendamos conocer el modelo en su totalidad, para lo que te pedimos dirigirte a las referencias o contacto de esta página.
+            </p>
+        </div>
+        <div className="contenedor-parrafodos">
+            <p>
+                 Podemos visualizar el Rango como una gran máquina de clasificación, un escáner de código de barras en un supermercado.
+                 El escáner lee el código de nueve dígitos de cada persona para determinar sus roles de grupo exluido o grupo privilegiado. Esto sucede muy rápido, automáticamente. El escáner no se detiene para investigar si la codificación realmente se ajusta al persona, simplemente elige una y continúa. El punto aquí es que el rango no es una identidad en evolución, es simplemente una categoría asignada o adscrita.<br></br>
+                
+            </p>
+        </div>
+        
+        <div>
+            <iframe src="https://giphy.com/embed/l1J9OTsRKPqjj2Y5q" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ciscoengemojis-security-l1J9OTsRKPqjj2Y5q"></a></p>
+        </div>
+
+        <div className='contenedor-parrafotres'>
+            <p>
+            La máquina fue diseñada y fabricada hace mucho tiempo para hacer solo una cosa; EXCLUIR al mayor número de personas por la menor razón posible. Todos tenemos múltiples membresías sociales, algunas llevan privilegio social, otras marginación. Las categorías NO son auto-elegidas y NO podemos renunciar a ellas.
+            </p>
+        </div>
+
+        <div className='contenedor-parrafo-cuatro'>
+            <p>
+                Como miembros de grupos sociales que son privilegiados, recibimos ventajas no ganadas y beneficios. Como miembros de grupos marginados nuestro acceso es limitado y nuestro movimiento restringido. Aunque nuestras vidas son afectadas profundamente por el Rango, estas categorías NO son verdaderas, es decir que aunque el mundo está organizado así, no es verdad que las razas existen y que unas son mejores que otras, o que las personas con discapacidad valen menos. Aún cuando estas membresías no son verdaderas, es necesario mirarlas para enfrentar la opresión que sufrimos y también la que ejercemos.
+            </p>
+        </div>
+
+        <div className='contenedor-parrafo-cinco'>
+            <p>
+                Las membresías en que somos clasificados y excluidos por la Máquina del Rango son 9: edad, cultura religiosa, etnicidad, clase social, orientación sexual, herencia indígena, nacionalidad, género y discapacidad. En el siguiente apartado hablaremos un poco más de la DISCAPACIDAD esperando entender las bases de este modelo, con respecto a esta categoría.
+            </p>
+        </div>
+        </section>
+
+        <section className='enlaces'>
+            <a href='/modelo'>ATRÁS</a>
+            <a href='/modelo/discapacidad'>SIGUIENTE -- La discapacidad como categoría de opresión</a>
+        </section>
         
         </>
     )
