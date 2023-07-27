0aa2466443129c5ee9f6f7f7d6c9964cbebca4c9
atzina <atzina@gmail.com>
2023-07-27 17:05:36 -0600

    nueva fuente títulos y otros detalles

--- tecnologia_y_discapacidad/src/components/ModAntiop/Discapacidad.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Discapacidad.jsx
@@ -12,7 +12,7 @@ function Discapacidad() {
       </div>
       <div className='contenedor-discapacidad'>
         <p>
-            La sociedad está orginizada para excluir y limitar las posibilidades de las personas con discapacidad, incluso las personas con discapacidades no visibles o consideradas &quot;leves&quot;.  La discacidad, a diferencia de la mayoría de la categorías de Rango, puede cambiar durante toda la vida. La pérdida de capacidad es intrínsecamente parte de le experiencia humana; una enfermedad, lesión o condición médica temporal puede convertir a cualquier persona en parte de un grupo marginado es decir infravalorado. La mayoría de las personas formaremos parte del grupo marginado por discapacidad en algún momento de nuestras vidas.
+            La sociedad está organizada para excluir y limitar las posibilidades de las personas con discapacidad, incluso las personas con discapacidades no visibles o consideradas &quot;leves&quot;.  La discacidad, a diferencia de la mayoría de la categorías de Rango, puede cambiar durante toda la vida. La pérdida de capacidad es intrínsecamente parte de le experiencia humana; una enfermedad, lesión o condición médica temporal puede convertir a cualquier persona en parte de un grupo marginado es decir infravalorado. La mayoría de las personas formaremos parte del grupo marginado por discapacidad en algún momento de nuestras vidas.
         </p>
 
         <p>
@@ -23,8 +23,16 @@ function Discapacidad() {
             La discapacidad tiene que ver con el efecto combiandao de la devaluación social además de las dificultades asociadas a la dificualtad en sí. Por ejemplo, no solo la falta de movilidad es lo que limita a las personas, sino un mundo construido exclusivamente para las personas que sin movilidad limitada. Lo opresivo es esta exclusión.
         </p>
 
+        <div className="capacitismo card">
+            <div className="card-body">
+              <h6 className="card-text">
+                La opresión asociada con esta categoría es el CAPACITISMO.
+              </h6>
+            </div>
+          </div>
+
         <p>
-            La opresión asociada con esta categoría es el Capacitismo.
+           
         </p>
         <p>
             La Capacidad, además de todas las categorías en que clasifica el rango, es una construcción y es defectuosa. Bajo el capacitismo todos limitamos nuestra definición de lo que significa ser humano a las personas capaces. Esto conduce a resistirse a tomar en consideración las necesidades del grupo marginado por el capacitismo.
@@ -33,6 +41,10 @@ function Discapacidad() {
             Un sintoma de esta resistencia es reconocer sólo la pérdida visible y pronunciada de pérdida de capacidad. Una posición antiopresiva sería reconocer que toda pérdida importa y el capacitismo afecta a todos los que experimentan una pérdida posible. El objetivo no es sugerir que la experiencia de pérdida de habilidad de cada persona es equivalente, si que la supremacía de habilidad afecta a todos los marginados por discapacidad.
         </p>
       </div>
+      <section className='enlaces'>
+            <a href='/modelo'>ATRÁS -- 1. La discapacidad como categoría de opresión</a>
+            <a href='/modelo/habilidades'>SIGUIENTE -- 3. Habilidades</a>
+        </section>
      
     </>
   );
--- tecnologia_y_discapacidad/src/components/ModAntiop/Modelo.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Modelo.jsx
@@ -4,7 +4,7 @@ function Modelo() {
   return (
     <>
       <div className="title-model">
-        <h1>Una mirada de antiopresión</h1>
+        <h2>Una mirada de antiopresión</h2>
       </div>
       <div className="principal-modelo conteiner">
         <aside>
@@ -19,30 +19,29 @@ function Modelo() {
         </aside>
 
         <div className="gral-dos conteiner-fluid">
-          
-            <section className="pregunta-uno col-12 col-md-12 col-lg-12">
-              <h3>¿Porqué incorporar un visión antiopresiva? </h3>
-              <p>
-                En términos generales este espacio, entendemos la opresión como
-                la tendencia a sobrevalorar a ciertos grupos y la desvaloración
-                de otros, produciendo ventajas para unos y desventajas para los
-                otros. La visión que busca esta propuesta tiene que ver con una
-                mirada antiopresiva que pueda ir más allá de la inclusión.
-              </p>
-            </section>
-            <section className="pregunta-dos col-12 col-md-12 col-lg-12 ">
-              <h3>¿Por que ir más allá de la inclusión?</h3>
-              <p>
-                La inclusión supone muchas veces, que los miembros de los grupos
-                marginados por la opresión se incorporen a las instituciones y
-                prácticas dominantes requiriendo que se asimilen a las normas
-                existentes. Es decir, se incluye a grupos marginados a nuestros
-                ambientes, exigiéndoles inconscientemente que ellos se las
-                arreglen y se vuelvan como nosotros, sin cuestionar las normas y
-                las estructuras que perpetuan la desigualdad.
-              </p>
-            </section>
-       
+          <section className="pregunta-uno col-12 col-md-12 col-lg-12">
+            <h3>¿Porqué incorporar un visión antiopresiva? </h3>
+            <p>
+              En términos generales este espacio, entendemos la opresión como la
+              tendencia a sobrevalorar a ciertos grupos y la desvaloración de
+              otros, produciendo ventajas para unos y desventajas para los
+              otros. La visión que busca esta propuesta tiene que ver con una
+              mirada antiopresiva que pueda ir más allá de la inclusión.
+            </p>
+          </section>
+          <section className="pregunta-dos col-12 col-md-12 col-lg-12 ">
+            <h3>¿Por que ir más allá de la inclusión?</h3>
+            <p>
+              La inclusión supone muchas veces, que los miembros de los grupos
+              marginados por la opresión se incorporen a las instituciones y
+              prácticas dominantes requiriendo que se asimilen a las normas
+              existentes. Es decir, se incluye a grupos marginados a nuestros
+              ambientes, exigiéndoles inconscientemente que ellos se las
+              arreglen y se vuelvan como nosotros, sin cuestionar las normas y
+              las estructuras que perpetuan la desigualdad.
+            </p>
+          </section>
+
           <div className="contenedor-modelo">
             <h3>El Modelo</h3>
             <p>
@@ -57,22 +56,28 @@ function Modelo() {
               conocer más , haz contacto.
             </p>
           </div>
-          <div className="cuadroInfo card" >
-          <div className="card-body">
-            <h5 className="card-title">Referencia de esta información</h5>
-            <p className="card-text">El contenido aquí vertido tiene como base en el libro: "Beyond incluson, Beyond Empowerment. A developmental Satrategy to Liberate Everyone", construido en muchos años de trabajo práctico y académico de Leticia Nieto. Para mas información:</p>
-            <a href="https://beyondinclusionbeyondempowerment.com/about-the-book/" className="card-link">web del libro</a>
-            
+          <div className="cuadroInfo card">
+            <div className="card-body">
+              <h5 className="card-title">Referencia de esta información</h5>
+              <p className="card-text">
+                El contenido aquí vertido tiene como base en el libro: "Beyond
+                incluson, Beyond Empowerment. A developmental Satrategy to
+                Liberate Everyone", construido en muchos años de trabajo
+                práctico y académico de Leticia Nieto. Para mas información:
+              </p>
+              <a
+                href="https://beyondinclusionbeyondempowerment.com/about-the-book/"
+                className="card-link"
+              >
+                web del libro
+              </a>
+            </div>
           </div>
-          
-          </div>
-         
         </div>
-         
       </div>
-        <div className="enlace-uno">
-            <a href="/modelo/rango">SIGUIENTE - 1. La Máquina del rango</a>
-        </div>
+      <div className="enlace-uno">
+        <a href="/modelo/rango">SIGUIENTE - 1. La Máquina del rango</a>
+      </div>
     </>
   );
 }
