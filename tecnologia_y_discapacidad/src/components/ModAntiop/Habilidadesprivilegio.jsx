1d0fed6e5af957a42d8e0cea7739f5b7f6bb2f96
atzina <atzina@gmail.com>
2023-07-29 14:39:49 -0600

    ocultar despues del parrafo dos y compartimentacion instrucciones

--- tecnologia_y_discapacidad/src/components/ModAntiop/Habilidadesprivilegio.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Habilidadesprivilegio.jsx
@@ -1,60 +1,32 @@
 import "./Habilidadesprivilegio.css";
 import { useState } from "react";
+import {Instrucciones} from "./Instrucciones";
 
 function Habilidadesprivilegio() {
   const [mostrarDivs, setMostrarDivs] = useState(true);
+  const [mostrarParrafo1, setMostrarParrafo1] = useState(true);
+  // const [mostrarParrafo2, setMostrarParrafo2] = useState(true);
 
   const toggleMostrarDivs = () => {
     setMostrarDivs(!mostrarDivs);
   };
+
+  const toogleMostrarParrafo1 = () => {
+    setMostrarParrafo1(!mostrarParrafo1);
+  };
+
+  // const toggleMOstrarParrafo2 = () => {
+  //   setMostrarParrafo2(!mostrarParrafo2);
+  // }
 
   return (
     <>
       <div className="title-habPriv">
         <h2>Habilidades Atiopresivas para personas sin discapacidad</h2>
       </div>
-      <div className="contenedor-instrucciones">
-        <p>
-          Bienvenida/o/e a este espacio para saber más de las habilidades
-          antiopresivas sobre la Discapacidad.
-        </p>
-        <p>
-          Al idenficarte como una persona sin discapacidad, lo más probable es
-          que seas miembro del Grupo Privilegiado. Aunque no seas conciente, tu
-          experiencia como persona capaz siempre ha estado facilitada por la
-          sociedad, y eres supervalorada/o/e frente al grupo de personas con
-          Discapacidad. Tu no elegiste esta membresía, te fue dada por la
-          sociedad en que vives, que oprime bajo el Capacitismo. Con seguridad
-          has participado inconscientemente en actos de discriminación contra
-          personas con Discapacidad. La propuesta de este modelo, te propone
-          hacer conciente tus privielgios para enriquecer tus habilidades y así,
-          construir una posibilidad de que tus acciones no opriman a nadie.
-        </p>
 
-        <p>
-          Con este ejercicio busco hacer un acercamiento a tus posibles
-          habilidades, para identificarte en algún sitio del esquema que propone
-          este modelo, y así saber un posible lugar donde ubicarte para por
-          donde empezar a trabajarte. Es a partir de tu propia experiencia que
-          te propongo conocer el modelo, y así saber las características
-          fundamentales de esta propuesta.
-        </p>
-
-        <p>
-          Después de hacer el ejercicio puedes observar abajo el esquema
-          completo de habilidades. Como he repetido a lo largo de esta
-          exposición, este trabajo busca solo ser informativo, el tema que nos
-          ocupa es muy complejo, si quieres identificar con claridad tus
-          habilidades y trabajar para mejorarlas acércate a saber más del modelo
-          con su creadora, o escribe al contacto de esta página.
-        </p>
-
-        <p>
-          A continuación te presentamos algunas habilidades referidas a la
-          Discapacidad, escoge la o las que más se acerquen a tu experiencia y
-          sentir.
-        </p>
-      </div>
+      <Instrucciones/>
+  
       <div className="contendor-hab-priviegio">
         <p>
           No sé mucho de las personas con discapacidad, no tengo problema con
@@ -64,107 +36,136 @@ function Habilidadesprivilegio() {
         <div>
           <button onClick={toggleMostrarDivs}>
             {" "}
-            {mostrarDivs ? "Ocultar habilidades" : "Mostrar habilidad"}{" "}
+            {mostrarDivs ? "Mostrar mas" : "Seguir con las otras"}{" "}
           </button>
         </div>
+
         {mostrarDivs && (
-          <><div className="parrafo-div">
-            <p>
-              Los respeto mucho pero yo no podría salir con una persona con
-              discapacidad
-            </p>
-          </div><div>
+          <>
+            <div className="hab-uno">
               <p>
-                No he tenido mucho contacto con personas con discapaciad, pero
-                sobretodo creo que todas son personas muy tiernas.
+                Los respeto mucho pero yo no podría salir con una persona con
+                discapacidad
               </p>
-            </div><div>
-              <p>
-                Ha sido difícil llegar hasta este punto de la información,
-                honestamente me aburre un poco.
-              </p>
-            </div><div>
-              <p>
-                Creo que todos somos iguales, no debería haber diferencias entre las
-                condiciones de mi compañero de trabajo y las mías, sus
-                &quot;privilegios&quot; me molestan un poco.
-              </p>
-            </div><div>
-              <p>
-                Creo en la inclusión, pero me enoja un poco que mi compañero con
-                discapacidad falte tanto al trabajo
-              </p>
-            </div><di>
-              <p>
-                Creo que la depresión es solo una forma de llamar la atención y la
-                forma en que las personas evitan sus responsabilidades
-              </p>
-            </di><div>
-              <p>
-                Yo sé lo que las personas con dispacadidad necesitan hacer, yo he
-                vivido situaciones muy dificiles y las he superado así.
-              </p>
-            </div><div>
-              <p>
-                Quisera ser la voz de las persons con dispacidad, no soy cercano, a
-                ninguna pero quisiera decir lo que ellos no pueden decir
-              </p>
-            </div><div>
-              <p>
-                Siento mucha incomodidad darme cuenta de como mi actuar afecta a las
-                personas con discapacidad
-              </p>
-            </div><div>
-              <p>
-                Siento culpa de no saber a todo lo que se tiene que enfrentar una
-                persona con autismo
-              </p>
-            </div><div>
-              <p>
-                Siento verguenza de haber sentido miedo de una persona con
-                discapacidad mental
-              </p>
-            </div><div>
-              <p>
-                Ahora me doy cuenta de qué no tengo idea de cómo te trata el mundo
-                cuando tienes una discapacidad. ¿Me puedes decir más?
-              </p>
-            </div><div>
-              <p>
-                Sé que no te entiendo, pero no sé qué hacer para ayudarte, voy a
-                pensar más sobre eso
-              </p>
-            </div><div>
-              <p>
-                Ahora entiendo que nunca voy a poder entender a cabalidad la
-                experiencia de una persona con Autismo
-              </p>
-            </div><div>
-              <p>
-                Decidí dedicar mi vida a hacer cosas para ayudar a las personas con
-                discapaciad
-              </p>
-            </div><div>
-              <p>
-                Hago labor para que más personas sin discapacidad, entiendan sus
-                privilegios y sus propias discriminaciones hacia las personas con
-                discapacidad.
-              </p>
-            </div><div>
-              <p>
-                Escucho y entiendo lo que las personas con discpacidad me dicen, no
-                impongo mis ideas de lo que es mejor para ellas y a hago acciones a
-                partir de sus propias sugerencias e intereses
-              </p>
-            </div><div>
-
-
-              <p>
-                Uso mis propios privielgios para que las personas con discapaciad sean
-                escuchadas, no hablo por ellas, genero espacios para que sus propias
-                voces sean posibles.
-              </p>
-            </div></>
+              <button onClick={toogleMostrarParrafo1}>
+                {mostrarParrafo1 ? "Mostrar mas" : "Seguir con las otras"}{" "}
+              </button>
+            </div>
+            {mostrarParrafo1 && (
+              <>
+                <div className="hab-dos">
+                  <p>
+                    No he tenido mucho contacto con personas con discapaciad,
+                    pero sobretodo creo que todas son personas muy tiernas.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Ha sido difícil llegar hasta este punto de la información,
+                    honestamente me aburre un poco.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Creo que todos somos iguales, no debería haber diferencias
+                    entre las condiciones de mi compañero de trabajo y las mías,
+                    sus &quot;privilegios&quot; me molestan un poco.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Creo en la inclusión, pero me enoja un poco que mi compañero
+                    con discapacidad falte tanto al trabajo
+                  </p>
+                </div>
+                <di>
+                  <p>
+                    Creo que la depresión es solo una forma de llamar la
+                    atención y la forma en que las personas evitan sus
+                    responsabilidades
+                  </p>
+                </di>
+                <div>
+                  <p>
+                    Yo sé lo que las personas con dispacadidad necesitan hacer,
+                    yo he vivido situaciones muy dificiles y las he superado
+                    así.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Quisera ser la voz de las persons con dispacidad, no soy
+                    cercano, a ninguna pero quisiera decir lo que ellos no
+                    pueden decir
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Siento mucha incomodidad darme cuenta de como mi actuar
+                    afecta a las personas con discapacidad
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Siento culpa de no saber a todo lo que se tiene que
+                    enfrentar una persona con autismo
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Siento verguenza de haber sentido miedo de una persona con
+                    discapacidad mental
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Ahora me doy cuenta de qué no tengo idea de cómo te trata el
+                    mundo cuando tienes una discapacidad. ¿Me puedes decir más?
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Sé que no te entiendo, pero no sé qué hacer para ayudarte,
+                    voy a pensar más sobre eso
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Ahora entiendo que nunca voy a poder entender a cabalidad la
+                    experiencia de una persona con Autismo
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Decidí dedicar mi vida a hacer cosas para ayudar a las
+                    personas con discapaciad
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Hago labor para que más personas sin discapacidad, entiendan
+                    sus privilegios y sus propias discriminaciones hacia las
+                    personas con discapacidad.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Escucho y entiendo lo que las personas con discpacidad me
+                    dicen, no impongo mis ideas de lo que es mejor para ellas y
+                    a hago acciones a partir de sus propias sugerencias e
+                    intereses
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Uso mis propios privielgios para que las personas con
+                    discapaciad sean escuchadas, no hablo por ellas, genero
+                    espacios para que sus propias voces sean posibles.
+                  </p>
+                </div>
+              </>
+            )}
+          </>
         )}
       </div>
     </>
