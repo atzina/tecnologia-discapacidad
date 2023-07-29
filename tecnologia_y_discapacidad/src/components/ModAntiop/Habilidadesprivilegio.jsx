4def569994fa4bcf68cc332b6b73d8c203af23c2
atzina <atzina@gmail.com>
2023-07-28 22:51:28 -0600

    intento para desaparecer el resto de preguntas

--- tecnologia_y_discapacidad/src/components/ModAntiop/Habilidadesprivilegio.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Habilidadesprivilegio.jsx
@@ -1,93 +1,148 @@
-import './Habilidadesprivilegio.css'
+import "./Habilidadesprivilegio.css";
 
-function Habilidadesprivilegio () {
-    return (
-        <>
-         <div className="title-habPriv">
-          <h2>Habilidades Atiopresivas para personas sin discapacidad</h2>
-         </div>
-         <div className='contenedor-instrucciones'>
+function Habilidadesprivilegio() {
 
-            <p>
-                Bienvenida/o/e a este espacio para saber más de las habilidades antiopresivas sobre la Discapacidad.
-            </p>
-            <p>  
-                Al idenficarte como una persona sin discapacidad, lo más probable es que seas miembro del Grupo Privilegiado. Aunque no seas conciente, tu experiencia como persona capaz siempre ha estado facilitada por la sociedad, y eres supervalorada/o/e  frente al grupo de personas con Discapacidad. Tu no elegiste esta membresía, te fue dada por la sociedad en que vives, que oprime bajo el Capacitismo. Con seguridad has participado inconscientemente en actos de discriminación contra personas con Discapacidad. La propuesta de este modelo, te propone hacer conciente tus privielgios para enriquecer tus habilidades y así, construir una posibilidad de que tus acciones no opriman a nadie.
-            </p>
+     function Mostrar () {
+        document.getElementById('pregunta-uno')
+     }
 
-            <p>
-                Con este ejercicio busco hacer un acercamiento a tus posibles habilidades, para identificarte en algún sitio del esquema que propone este modelo, y así saber un posible lugar donde ubicarte para por donde empezar a trabajarte. Es a partir de tu propia experiencia que te propongo conocer el modelo, y así saber las características fundamentales de esta propuesta.
-            </p>
+  return (
+    <>
+      <div className="title-habPriv">
+        <h2>Habilidades Atiopresivas para personas sin discapacidad</h2>
+      </div>
+      <div className="contenedor-instrucciones">
+        <p>
+          Bienvenida/o/e a este espacio para saber más de las habilidades
+          antiopresivas sobre la Discapacidad.
+        </p>
+        <p>
+          Al idenficarte como una persona sin discapacidad, lo más probable es
+          que seas miembro del Grupo Privilegiado. Aunque no seas conciente, tu
+          experiencia como persona capaz siempre ha estado facilitada por la
+          sociedad, y eres supervalorada/o/e frente al grupo de personas con
+          Discapacidad. Tu no elegiste esta membresía, te fue dada por la
+          sociedad en que vives, que oprime bajo el Capacitismo. Con seguridad
+          has participado inconscientemente en actos de discriminación contra
+          personas con Discapacidad. La propuesta de este modelo, te propone
+          hacer conciente tus privielgios para enriquecer tus habilidades y así,
+          construir una posibilidad de que tus acciones no opriman a nadie.
+        </p>
 
-            <p>
-                Después de hacer el ejercicio puedes observar abajo el esquema completo de habilidades. Como he repetido a lo largo de esta exposición, este trabajo busca solo ser informativo, el tema que nos ocupa es muy complejo,  si quieres identificar con claridad tus habilidades y trabajar para mejorarlas acércate a saber más del modelo con su creadora, o escribe al contacto de esta página. 
-            </p>
+        <p>
+          Con este ejercicio busco hacer un acercamiento a tus posibles
+          habilidades, para identificarte en algún sitio del esquema que propone
+          este modelo, y así saber un posible lugar donde ubicarte para por
+          donde empezar a trabajarte. Es a partir de tu propia experiencia que
+          te propongo conocer el modelo, y así saber las características
+          fundamentales de esta propuesta.
+        </p>
 
-            <p>
-                A continuación te presentamos algunas habilidades referidas a la Discapacidad, escoge la o las que más se acerquen a tu experiencia y sentir.
-            </p>
-         </div>
-         <div className='contendor-hab-priviegio'>
-            <p>
-                No sé mucho de las personas con discapacidad, no tengo problema con ser incluyente, pero sinceramente sería muy difícil para mí que una persona con discapacidad saliera con mi hija o hijo
-            </p>
-            <p>
-                Los respeto mucho pero yo no podría salir con una persona con discapacidad
-            </p>
-            <p>
-                No he tenido mucho contacto con personas con discapaciad, pero creo que todas son personas muy tiernas.
-            </p>
-            <p>
-                Ha sido difícil llegar hasta este punto de la información, honestamente me aburre un poco.
-            </p>
-            <p>
-                Creo que todos somos iguales, no debería haber diferencias entre las condiciones de mi compañero de trabajo y las mías, sus &quot;privilegios&quot;  me molestan un poco.
-            </p>
-            <p>
-                Creo en la inclusión, pero me enoja un poco que mi compañero con discapacidad falte tanto al trabajo
-            </p>
-            <p>
-                Creo que la depresión es solo una forma de llamar la atención y la forma en que las personas evitan sus responsabilidades
-            </p>
-            <p>
-                Yo sé lo que las personas con dispacadidad necesitan hacer, yo he vivido situaciones muy dificiles y las he superado así.
-            </p>
-            <p>
-                Quisera ser la voz de las persons con dispacidad, no soy cercano, a ninguna pero quisiera decir lo que ellos no pueden decir
-            </p>
-            <p>
-                Siento mucha incomodidad darme cuenta de como mi actuar afecta a las personas con discapacidad
-            </p>
-            <p>
-                Siento culpa de no saber a todo lo que se tiene que enfrentar una persona con autismo
-            </p>
-            <p>
-                Siento verguenza de haber sentido miedo de una persona con discapacidad mental 
-            </p>
-            <p>
-                Ahora me doy cuenta de qué no tengo idea de cómo te trata el mundo cuando tienes una discapacidad. ¿Me puedes decir más?
-            </p>
-            <p> 
-                Sé que no te entiendo, pero no sé qué hacer para ayudarte, voy a pensar más sobre eso
-            </p>
-            <p>
-                Ahora entiendo que nunca voy a poder entender a cabalidad la experiencia de una persona con Autismo
-            </p>
-            <p>
-                Decidí dedicar mi vida a hacer cosas para ayudar a las personas con discapaciad
-            </p>
-            <p>
-                Hago labor para que más personas sin discapacidad, entiendan sus privilegios y sus propias discriminaciones hacia las personas con discapacidad.
-            </p>
-            <p>
-                Escucho y entiendo lo que las personas con discpacidad me dicen, no impongo mis ideas de lo que es mejor para ellas y a hago acciones a partir de sus propias sugerencias e intereses
-            </p>
-            <p>
-                Uso mis propios privielgios para que las personas con discapaciad sean escuchadas, no hablo por ellas, genero espacios para que sus propias voces sean posibles.
-            </p>
-         </div>
-        </>
-    )
+        <p>
+          Después de hacer el ejercicio puedes observar abajo el esquema
+          completo de habilidades. Como he repetido a lo largo de esta
+          exposición, este trabajo busca solo ser informativo, el tema que nos
+          ocupa es muy complejo, si quieres identificar con claridad tus
+          habilidades y trabajar para mejorarlas acércate a saber más del modelo
+          con su creadora, o escribe al contacto de esta página.
+        </p>
+
+        <p>
+          A continuación te presentamos algunas habilidades referidas a la
+          Discapacidad, escoge la o las que más se acerquen a tu experiencia y
+          sentir.
+        </p>
+      </div>
+      <div className="contendor-hab-priviegio">
+        <p>
+          No sé mucho de las personas con discapacidad, no tengo problema con
+          ser incluyente, pero sinceramente sería muy difícil para mí que una
+          persona con discapacidad saliera con mi hija o hijo
+        </p>
+        <div>
+          <button onClick={()=> Mostrar()}> Mostrar habilidad</button>
+        </div>
+        <div id="despues-pregunta-uno"className="despues-pregunta-uno">
+          <p>
+            Los respeto mucho pero yo no podría salir con una persona con
+            discapacidad
+          </p>
+          <p>
+            No he tenido mucho contacto con personas con discapaciad, pero
+            sobretodo creo que todas son personas muy tiernas.
+          </p>
+          <p>
+            Ha sido difícil llegar hasta este punto de la información,
+            honestamente me aburre un poco.
+          </p>
+          <p>
+            Creo que todos somos iguales, no debería haber diferencias entre las
+            condiciones de mi compañero de trabajo y las mías, sus
+            &quot;privilegios&quot; me molestan un poco.
+          </p>
+          <p>
+            Creo en la inclusión, pero me enoja un poco que mi compañero con
+            discapacidad falte tanto al trabajo
+          </p>
+          <p>
+            Creo que la depresión es solo una forma de llamar la atención y la
+            forma en que las personas evitan sus responsabilidades
+          </p>
+          <p>
+            Yo sé lo que las personas con dispacadidad necesitan hacer, yo he
+            vivido situaciones muy dificiles y las he superado así.
+          </p>
+          <p>
+            Quisera ser la voz de las persons con dispacidad, no soy cercano, a
+            ninguna pero quisiera decir lo que ellos no pueden decir
+          </p>
+          <p>
+            Siento mucha incomodidad darme cuenta de como mi actuar afecta a las
+            personas con discapacidad
+          </p>
+          <p>
+            Siento culpa de no saber a todo lo que se tiene que enfrentar una
+            persona con autismo
+          </p>
+          <p>
+            Siento verguenza de haber sentido miedo de una persona con
+            discapacidad mental
+          </p>
+          <p>
+            Ahora me doy cuenta de qué no tengo idea de cómo te trata el mundo
+            cuando tienes una discapacidad. ¿Me puedes decir más?
+          </p>
+          <p>
+            Sé que no te entiendo, pero no sé qué hacer para ayudarte, voy a
+            pensar más sobre eso
+          </p>
+          <p>
+            Ahora entiendo que nunca voy a poder entender a cabalidad la
+            experiencia de una persona con Autismo
+          </p>
+          <p>
+            Decidí dedicar mi vida a hacer cosas para ayudar a las personas con
+            discapaciad
+          </p>
+          <p>
+            Hago labor para que más personas sin discapacidad, entiendan sus
+            privilegios y sus propias discriminaciones hacia las personas con
+            discapacidad.
+          </p>
+          <p>
+            Escucho y entiendo lo que las personas con discpacidad me dicen, no
+            impongo mis ideas de lo que es mejor para ellas y a hago acciones a
+            partir de sus propias sugerencias e intereses
+          </p>
+          <p>
+            Uso mis propios privielgios para que las personas con discapaciad
+            sean escuchadas, no hablo por ellas, genero espacios para que sus
+            propias voces sean posibles.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Habilidadesprivilegio;
