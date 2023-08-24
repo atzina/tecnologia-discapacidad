bfab79decb473270ceef8c558e685ef3a46e232d
atzina <atzina@gmail.com>
2023-08-23 19:29:28 -0600

    estilos habilidades grupo privilegiado

--- tecnologia_y_discapacidad/src/components/ModAntiop/Distanciamiento.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Distanciamiento.jsx
@@ -1,27 +1,33 @@
+import './Distanciamiento.css'
+
 function Distanciamiento() {
   return (
     <>
-      <a name="ancla-distanciamiento"></a>
-      <h2>Habilidades de Distanciamiento</h2>
-      <p>Postura física: Extender los brazos, con las palmas hacia afuera y hacia abajo, como si empujara algo hacia abajo o tapara algo</p>
-      <p>Sentimiento/Sensación: Desdén, repugnancia. </p>
-      <p>Si te has identificado con esta frase, es muy probable que te encuentres en este conjunto de habilidades</p>
+      <div className='contenedor-distanciamiento'>
+        <a name="ancla-distanciamiento"></a>
+        <h2>Habilidades de Distanciamiento</h2>
+        <p>
+          Postura física: Extender los brazos, con las palmas hacia afuera y hacia abajo, como si empujara algo hacia abajo o tapara algo
+        </p>
+        <p>Sentimiento/Sensación: Desdén, repugnancia. </p>
+        <p>Si te has identificado con esta frase, es muy probable que te encuentres en este conjunto de habilidades</p>
 
-      <p>
-        Estando en distanciamiento nos damos cuenta de la desigualgualdad y quiza un poco de la opresión que ejercemos a las personas con discapcidad pero reaccionamos alejándonos. No alejamos del conocimiento y la conciencia de la opresión, porque no sabemos que hacer con las implicaciones que esto puede traer a nosotros mismos.
-      </p>
+        <p>
+          Estando en distanciamiento nos damos cuenta de la desigualgualdad y quiza un poco de la opresión que ejercemos a las personas con discapcidad pero reaccionamos alejándonos. No alejamos del conocimiento y la conciencia de la opresión, porque no sabemos que hacer con las implicaciones que esto puede traer a nosotros mismos.
+        </p>
 
-      <p>
-        Hay muchas formas en que los grupos privilegiados nos defendemos del contacto con las personas con discapacidad, por ejemplo podemos no tener problema en nuestras mentes con estas personas pero preferimos que no estén cerca de nosotros, podemos llegar a moslestarnos de que alguna persona con discpacidad nos saque de nuestra normalidad o zona de confort. Es también distanciamiento cuando las personas elaboran discursos de odio. Otra forma de distanciamiento puede ser una evalucación positiva pero estereotipada de las personas con discapcidad. A menudo podemos tomar con mucha facilidad una postura paternalista hacia las personas con discapacidad.
-      </p>
+        <p>
+          Hay muchas formas en que los grupos privilegiados nos defendemos del contacto con las personas con discapacidad, por ejemplo podemos no tener problema en nuestras mentes con estas personas pero preferimos que no estén cerca de nosotros, podemos llegar a moslestarnos de que alguna persona con discpacidad nos saque de nuestra normalidad o zona de confort. Es también distanciamiento cuando las personas elaboran discursos de odio. Otra forma de distanciamiento puede ser una evalucación positiva pero estereotipada de las personas con discapcidad. A menudo podemos tomar con mucha facilidad una postura paternalista hacia las personas con discapacidad.
+        </p>
 
-      <p>
-         Esta habilidad nos permite mantenernos seuros y proteger nuestro espacios de cualquier información sobre las condiciones en que las personas viven y sobre nuestros privilegios sobre ellas. Usando estas habilides hacemos poco esfuerzo para aprender sobre los problemas de las personas con discapcidad, no buscamos involucrarnos ni saber más de ellos.
-      </p>
+        <p>
+          Esta habilidad nos permite mantenernos seuros y proteger nuestro espacios de cualquier información sobre las condiciones en que las personas viven y sobre nuestros privilegios sobre ellas. Usando estas habilides hacemos poco esfuerzo para aprender sobre los problemas de las personas con discapcidad, no buscamos involucrarnos ni saber más de ellos.
+        </p>
 
-      <p>
-        Para desarrollar habilidades que te lleven al próximo nivel, es necesario que concozcas a personas con discapacidad y a las personas que trabajan con ellas, que puedas escucharlas y aprendas de sus inquietudes e ideas, así puedes abrir posibilidades a la inlcusión
-      </p>
+        <p>
+          Para desarrollar habilidades que te lleven al próximo nivel, es necesario que concozcas a personas con discapacidad y a las personas que trabajan con ellas, que puedas escucharlas y aprendas de sus inquietudes e ideas, así puedes abrir posibilidades a la inlcusión
+        </p>
+      </div>
 
     </>
   );
