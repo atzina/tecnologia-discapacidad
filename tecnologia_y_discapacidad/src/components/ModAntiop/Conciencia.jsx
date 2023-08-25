48def860372b81e5463fdc31517940605fd29f19
atzina <atzina@gmail.com>
2023-08-24 22:23:23 -0600

    conjunto habilidades conciencia con estilo básico

--- tecnologia_y_discapacidad/src/components/ModAntiop/Conciencia.jsx
+++ tecnologia_y_discapacidad/src/components/ModAntiop/Conciencia.jsx
@@ -1,9 +1,30 @@
+import './Conciencia.css'
+
 function Conciencia() {
   return (
     <>
-      <a name="ancla-conciencia"></a>
-      <h2>Habilidades de Conciencia</h2>
+      <div className='contenedor-conciencia'>
+        <a name="ancla-conciencia"></a>
+        <h2>Habilidades de Conciencia</h2>
+        <p>
+          Postura física: sentarse con la espalda recta al borde del asiento, escuchando, inclinados.
+        </p>
+        <p>
+          Sentimiento/Sensación: Culpa y verguenza.
+        </p>
+        <p>
+          Es dificil alcanzar el uso de las habilidades de Conciencia. Es duro e incómodo. Llegar a estas habilidades puede sentirse como una gran revelación, un gran &quot;darse cuenta&quot;.
+          Es darse cuenta que el mundo es inusto, y que a las personas sin discapacidad nos beneficia de maneras que nisiquiera podemos imaginar. En que existe una relación directa entre el sufrimiento de aquellos que están marginados y la facilidad de nuestras vidas, no importa que no lo sintamos así.
+        </p>
+        <p>
+          Con las habilidades de conciencia podemos reconocer la diferencia, y entendemos que la opresión trabaja de manera sitemática y compleja. Podemos hacer preguntas respetuosas y escuchar realmente las respuestas. Entendemos que las personas con discapacidad son expertos en su propia existencia y podemos dejar de lado nuestras propias perspectivas para escuchar lo que tiene que decir.
+        </p>
+        <p>
+          Empezar a ver la realidad de la opresión es avasallador, a menudo podemos sentir parálsisis al reflexionar sobre nuestros privilegios. La capacidad de tolerar sentimientos incómodos es esencial para desarrollar y mantener la conciencia. Se necesita mucho compromiso para tolerar las habilidades de Conciencia, solo practicando podemos desarrollar  la Alianza; practicando la escucha verdadera a los demas, una y otra vez, después de esto podemos darnos cuenta que es posible cambiar la situación y pasar al siguiente conjunto de habilidades.
+        </p>
+      </div>
     </>
+
   );
 }
 
